0a79fc36124d9a20cd9f430d863c7c055150c378
jjliang1005 <jia-jing.liang@insa-lyon.fr>
2015-02-20 16:11:30 -0500

    init

---
+++ TP2_LocalisationMap/app/src/main/java/com/polymtl/jiajing/tp2_localisationmap/ItineraryService.java
@@ -0,0 +1,16 @@
+package com.polymtl.jiajing.tp2_localisationmap;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ItineraryService extends Service {
+    public ItineraryService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ TP2_LocalisationMap/app/src/main/java/com/polymtl/jiajing/tp2_localisationmap/MarkerNetwork.java
@@ -0,0 +1,7 @@
+package com.polymtl.jiajing.tp2_localisationmap;
+
+/**
+ * Created by Jiajing on 2015/2/20.
+ */
+public class MarkerNetwork extends Marker{
+}
